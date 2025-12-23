76c71891b7afbf0dec4fce1708b7dcfef4abc51e
Kee-Sharp <keetwosuccess@gmail.com>
2025-12-22 21:16:33 -0500

    adding vis-timeline package

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect, useRef } from 'react';
+import { DataSet } from 'vis-data';
+import { Timeline } from 'vis-timeline';
+import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const timelineRef = useRef<Timeline>(null);
+  useEffect(() => {
+    const container = document.getElementById('visualization');
+    const items = new DataSet([
+      { id: 1, content: 'item 1', start: '2025-04-20' },
+      { id: 2, content: 'item 2', start: '2025-04-14' },
+      { id: 3, content: 'item 3', start: '2025-04-18' },
+      { id: 4, content: 'item 4', start: '2025-04-16', end: '2025-04-19' },
+      { id: 5, content: 'item 5', start: '2025-04-25' },
+      { id: 6, content: 'item 6', start: '2025-04-27', type: 'point' },
+    ]);
+    const timeline = new Timeline(container!, items, {
+      showMajorLabels: true,
+      showMinorLabels: true,
+      format: { minorLabels: { day: 'ddd D' } },
+    });
+    timelineRef.current = timeline;
+    console.log('Timeline initialized', timeline);
+    return () => {
+      if (timelineRef.current) {
+        timelineRef.current.destroy();
+        console.log('Timeline destroyed');
+      }
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div style={{ width: '100%', height: '1000px' }}>
+      <p>Task Master Timeline</p>
+      <div id='visualization' />
+    </div>
   );
 }
 
