6618f91402f253b6b30bf94aded171950c4a537a
Kee-Sharp <keetwosuccess@gmail.com>
2025-12-25 05:14:43 -0500

    gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/task-master/',
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+});
